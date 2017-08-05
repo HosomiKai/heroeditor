ff25f434d0ae8ba4a7311b65b1c8c45ea802fa9b
姚嘉凱 <hosomikai@yaojiakaide-MacBook-Pro.local>
2017-08-05 11:14:13 +0800

    任務七 加入http從服務器獲取資料

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+// imports for loading &  configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -14,13 +19,15 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule,
   ],
   providers: [HeroService],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(private http: Http) { }
 
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl) // 回傳rxjs的observable對象
+               .toPromise()  // 將其轉回Promise對象
+               .then(response => response.json().data as Hero[]) // Extracting the data in the then callback
+               .catch(this.handleError); // catch服務器的失敗訊息
+  }
+
+  // 我們必須預料到http會請求失敗,因為有太多無法控制的原因可能導致他出現各種錯誤
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+    // 此範例將錯誤記到控制台中, 在真實世界中應該用代碼對錯誤進行處理
   }
 
   getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-               .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleError);
   }
-
 }
